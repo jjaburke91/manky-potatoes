0de60e93d8ee4dabb142c5ae790a29d002ef1ac0
Jamie Burke <jjaburke@hotmail.com>
2019-10-14 21:57:53 +0100

    init commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,37 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+import Film from "../Film/Film";
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+import films from "../../data/films";
+
+class FilmEntry {
+  constructor(row, isFirstRow) {
+    this.title = row[0];
+    this.year = row[1] || "";
+    this.director = row[14] || "";
+    this.reviewRating = row[16] || "";
+    this.reviewComment = row[17] || "";
+    this.firstRow = !!isFirstRow;
+  }
+}
+
+const IndexPage = () => {
+  const allFilms = [];
+  for (let i = 0; i < films.values.length; i++) {
+    if (i < 2) continue;
+    allFilms.push(new FilmEntry(films.values[i]))
+  }
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      {allFilms.map((film, i) => (
+        <Film film={film} key={i} isFirstRow={i == 0}/>
+      ))}
+    </Layout>
+  );
+}
 
 export default IndexPage
