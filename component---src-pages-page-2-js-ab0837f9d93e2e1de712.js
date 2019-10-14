1bc54d94893126bd816ccc0a48c56d795d92a5e0
Jamie Burke <jjaburke@hotmail.com>
2019-10-14 22:02:46 +0100

    Updates

---
+++ component---src-pages-page-2-js-ab0837f9d93e2e1de712.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{212:function(e,t,a){"use strict";a.r(t);var n=a(0),o=a.n(n),l=a(85),c=a(210),r=a(209);t.default=function(){return o.a.createElement(c.a,null,o.a.createElement(r.a,{title:"Page two"}),o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(l.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-ab0837f9d93e2e1de712.js.map
---
+++ styles-e3c7d3cbd8f2f5d40a7d.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{214:function(n,w,o){}}]);
+//# sourceMappingURL=styles-e3c7d3cbd8f2f5d40a7d.js.map
